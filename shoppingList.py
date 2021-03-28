a121f3fa72ad83dd47ff26f03ee0c9ed93f33b9f
hiddendragon1337 <79630482+hiddendragon1337@users.noreply.github.com>
2021-03-28 14:55:54 +1100

    Add files via upload

---
+++ shoppingList.py
@@ -0,0 +1,62 @@
+import pprint
+import pandas as pd
+
+recipes = {
+    "bryce1": {"tomato": 3, "red onion": 2, "passata": 1},
+    "sehrish1": {"salmon": 1, "apples": 2, "red onion": 1},
+    "bryce2": {"tomato": 1, "red capsicum": 1, "passata": 1},
+}
+
+shoppingList = {}
+while True:
+    print("Please enter the name of a recipe.")
+    print("Enter 'Help' for the list of recipes.")
+    print("Enter 'Done' to finish and produce the shopping list.")
+    recipeName = input()
+    if recipeName in recipes.keys():
+        shoppingList[recipeName] = recipes[recipeName]
+    elif recipeName == "Done":
+        print(shoppingList.keys())
+        break
+    elif recipeName == "Help":
+        print(recipes)
+    else:
+        print("Please enter a valid input.")
+
+finalList = {}
+for k, v in shoppingList.items():
+    for k1, v1, in v.items():
+        if k1 in finalList.keys():
+            finalList[k1] = finalList[k1] + v1
+        else:
+            finalList[k1] = v1
+
+frozen = ['frozen veg', 'frozen corn']
+pantry = ['black beans', 'chickpeas', 'passata']
+spices = ['cumin', 'coriander', 'smoked paprika', 'cajun', 'five spice']
+
+woolworths = ['tofu', 'brown rice noodles']
+
+
+
+
+
+print('Recipes chosen:')
+pprint.pprint(shoppingList)
+
+print('Final shopping list:')
+pprint.pprint(finalList)
+
+(pd.DataFrame.from_dict(data=finalList, orient='index')
+   .to_csv('dict_file.csv', header=False))
+
+
+
+
+
+
+
+
+
+
+# Write your code here :-)
